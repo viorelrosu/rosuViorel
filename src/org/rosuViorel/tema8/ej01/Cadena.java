05487c40f219d78aa591f5be60a4f3a0115c43f4
Viorel Rosu <viorel.rosu@gmail.com>
2020-05-18 20:11:14 +0200

    JavaDoc en el ejercicio 01 del tema 8

--- src/org/rosuViorel/tema8/ej01/Cadena.java
+++ src/org/rosuViorel/tema8/ej01/Cadena.java
@@ -11,18 +11,39 @@ public class Cadena {
 		return this.cadena;
 	}
 
+	/**
+	 * Método para concatenar una cadena de texto por delante.
+	 * 
+	 * @param cadenaAConcatenar
+	 */
 	public void cDelante(String cadenaAConcatenar)  {
 		this.cadena = cadenaAConcatenar + this.cadena;
 	}
 
-	public void cDetras(String cadena) {
-		this.cadena = this.cadena + cadena;
+	/**
+	 * Método para concatenar una cadena de texto en la parte de atras.
+	 * 
+	 * @param cadenaAConcatenar
+	 */
+	public void cDetras(String cadenaAConcatenar) {
+		this.cadena = this.cadena + cadenaAConcatenar;
 	}
 
+	/**
+	 * Método para quitar un carácter pasado como parámetro.
+	 * 
+	 * @param c es el carácter por quitar
+	 */
 	public void quitar(char c) {
 		this.cadena = this.cadena.replace("" + c + "", "");
 	}
 	
+	/**
+	 * Método para quitar un carácter pasado como parámetro tantas veces como se indica.
+	 * 
+	 * @param c es el carácter por quitar
+	 * @param i es el número de veces 
+	 */
 	public void quitar(char c, int i) {
 		int contador=0, contadorSubstring = 1;
 		for(int j=0; j<this.cadena.length(); j++) {
